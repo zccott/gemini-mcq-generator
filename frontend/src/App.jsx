3d36e75666bddd2f43330211ce2ef7ea375ea934
RAHUL A P <rahul.ap@aot-technologies.com>
2025-06-21 21:41:30 -0700

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import ClerkProviderWithRoutes from "./auth/ClerkProviderWithRoutes";
 import { Routes, Route } from "react-router-dom";
+import { Layout } from "./layout/layout.jsx";
+import { ChallengeGenerator } from "./challenge/ChallengeGenerator.jsx";
+import { HistoryPannel } from "./history/HistoryPannel.jsx";
+import { AutheticationPage } from "./auth/AutheticationPage.jsx"
 
 function App() {
-  return <ClerkProviderWithRoutes>
-    <Routes>
-      
-    </Routes>
-  </ClerkProviderWithRoutes>;
+  return (
+    <ClerkProviderWithRoutes>
+      <Routes></Routes>
+    </ClerkProviderWithRoutes>
+  );
 }
 
 export default App;
---
+++ frontend/src/auth/AutheticationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AutheticationPage = () => {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default AutheticationPage
